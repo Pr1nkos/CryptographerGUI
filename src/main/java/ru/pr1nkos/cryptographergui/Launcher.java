bce824eabdd95b87a933462a3d04cc2fa80e514f
Pr1nkos <prinkos1@gmail.com>
2024-02-23 20:20:25 +0300

    Compiled JAR

---
+++ src/main/java/ru/pr1nkos/cryptographergui/Launcher.java
@@ -0,0 +1,10 @@
+package ru.pr1nkos.cryptographergui;
+
+import javafx.application.Application;
+
+public class Launcher {
+	public static void main(String[] args) {
+		Application.launch(Main.class, args);
+	}
+
+}
--- src/main/java/ru/pr1nkos/cryptographergui/Main.java
+++ src/main/java/ru/pr1nkos/cryptographergui/Main.java
@@ -10,9 +10,6 @@ import java.io.IOException;
 
 
 public class Main extends Application {
-	public static void main(String[] args) {
-		launch();
-	}
 
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("ComplexApplication_css.fxml"));
